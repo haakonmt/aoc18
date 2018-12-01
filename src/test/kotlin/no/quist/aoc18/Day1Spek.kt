af6449a0c2ca0685fc0e956ce95731a8ad6892c7
Håkon Meyer Tørnquist <haakon.t@gmail.com>
2018-12-01 15:15:58 +0100

    Add missing tests to Day1Spek

--- src/test/kotlin/no/quist/aoc18/Day1Spek.kt
+++ src/test/kotlin/no/quist/aoc18/Day1Spek.kt
@@ -4,11 +4,13 @@ import no.quist.aoc18.days.Day1
 
 object Day1Spek : AbstractSpek<List<Int>, Int>(
     Day1::task1 to mapOf(
+        listOf(1, -2, 3, 1) to 3,
         listOf(1, 1, 1) to 3,
         listOf(1, 1, -2) to 0,
         listOf(-1, -2, -3) to -6
     ),
     Day1::task2 to mapOf(
+        listOf(1, -2, 3, 1, 1, -2) to 2,
         listOf(1, -1) to 0,
         listOf(3, 3, 4, -2, -4) to 10,
         listOf(-6, 3, 8, 5, -6) to 5,
