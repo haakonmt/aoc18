f663391f4521ae3b8bb2afbbdaeac6113023d89c
Håkon Meyer Tørnquist <haakon.t@gmail.com>
2018-12-01 14:42:55 +0100

    Refactor for readability

--- src/test/kotlin/no/quist/aoc18/AbstractSpek.kt
+++ src/test/kotlin/no/quist/aoc18/AbstractSpek.kt
@@ -3,11 +3,13 @@ package no.quist.aoc18
 import org.spekframework.spek2.Spek
 import kotlin.test.assertEquals
 
-abstract class AbstractSpek<I, R>(vararg tests: Pair<(I) -> R, Map<I, R>>) : Spek({
-    tests.forEachIndexed { groupIndex, (testRunner, tests) ->
-        group("Task ${groupIndex + 1}") {
-            tests.keys.forEachIndexed { testIndex, input ->
-                test("Test ${testIndex + 1}") {
+typealias TestGroup<I, R> = Pair<(I) -> R, Map<I, R>>
+
+abstract class AbstractSpek<I, R>(vararg groups: TestGroup<I, R>) : Spek({
+    groups.forEachIndexed { gIndex, (testRunner, tests) ->
+        group("Task ${gIndex + 1}") {
+            tests.keys.forEachIndexed { tIndex, input ->
+                test("Test ${tIndex + 1}") {
                     assertEquals(tests[input], testRunner(input))
                 }
             }
